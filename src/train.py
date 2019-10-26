10cee27e79b26f8ef18992a96aba3e755bc0ed84
huutrinh68 <trinhsp89@gmail.com>
2019-10-26 09:54:35 +0900

    fix bug

--- src/train.py
+++ src/train.py
@@ -13,6 +13,7 @@ import src.factory as factory
 #  
 
 # common setting --------------------------------
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ['CUDA_VISIBLE_DEVICES']='0,1,2,3'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
