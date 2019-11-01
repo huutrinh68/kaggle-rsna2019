e792adad17708b707b333a2678eda814f4df8d96
trinhnh1 <huutrinh@huutrinhs-MacBook-Pro.local>
2019-11-01 23:28:11 +0900

    resum from epoch 14

--- bin/train001.sh
+++ bin/train001.sh
@@ -5,6 +5,6 @@ fold=0
 conf=./conf/${model}.py
 n_tta=1
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=1,2,3
 
 python -m src.train ${mode} ${conf} --fold ${fold} --gpu ${gpu} --n_tta ${n_tta}
